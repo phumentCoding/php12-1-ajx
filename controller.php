62b2f01e44060ab0751617f18b25a65af44f74bb
phumentCoding <phumentceo@gmail.com>
2025-05-13 13:08:20 +0700

    update

--- controller.php
+++ controller.php
@@ -10,19 +10,36 @@
 
       case 'select' : {
 
+         try{
+            $sql = "SELECT * FROM students";
+            $result = mysqli_query($conn,$sql);
 
-        //response to index.php
-        echo json_encode([
-            'status' => true,
-            'message' => 'select all student'
-        ]);
+            if($result){
+               $students = mysqli_fetch_all($result,MYSQLI_ASSOC);
+            }else{
+               $students = [];
+            }
 
-        /*
-        {
-           "status" : true,
-           "message" : "select all student"
-        }
-        */
+
+            http_response_code(200);
+
+            //response to index.php
+            echo json_encode([
+               'status' => true,
+               'message' => 'select all student',
+               'students' => $students
+            ]);
+
+
+         }catch(Exception $e){
+
+            http_response_code(500);
+
+            echo json_encode([
+               'status' => false,
+               'errors' => $e->getMessage()
+            ]);
+         }
 
         break;
       }
